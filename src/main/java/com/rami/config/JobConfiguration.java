da2e7c3c42c7aa4a420b99323716db20c0545dd7
rami stefanidis <rami.stefanidis@gmail.com>
2018-11-25 17:38:23 -0500

    Stateful Job implementation

--- src/main/java/com/rami/config/JobConfiguration.java
+++ src/main/java/com/rami/config/JobConfiguration.java
@@ -17,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 
 import java.util.ArrayList;
 import java.util.List;
